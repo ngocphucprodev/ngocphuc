b677b1252e69c50c626c85acfe5ee82d511a5093
ngocphucprodev <59273340+ngocphucprodev@users.noreply.github.com>
2020-01-14 23:29:41 +0700

    Add files via upload

--- snakeconsole.cpp
+++ snakeconsole.cpp
@@ -7,8 +7,7 @@
 #include"Mylib.h"
 #define MAX 100
 using namespace std;
-int nub = 4;
-
+int nub = 6;
 class View {
 private:
 	static const int height = 20;
@@ -16,8 +15,7 @@ private:
 public:
 	void menu();
 	void menuchon(int chon, int choncu);
-	void create_lineabove();
-	void create_lineunder();
+	void create_lineabove();	void create_lineunder();
 	void create_lineleft();
 	void create_lineright();
 	void create_snack(int toadox[], int toadoy[]);
@@ -26,17 +24,10 @@ public:
 	void process_snack(int toadox[], int toadoy[], int x, int y);
 	void Add(int a[], int x);
 	void Delete(int a[], int vt);
-};
-class Snake {
-private :
-	int toadox[], toadoy[];
-public :
-	void create_snack(int toadox[], int toadoy[]);
-	void draw_snack(int toadox[], int toadoy[]);
-	void Delete_snack(int toadox[], int toadoy[]);
-	void process_snack(int toadox[], int toadoy[], int x, int y);
-	void Add(int a[], int x);
-	void Delete(int a[], int vt);
+	bool ktran(int x0, int y0);
+	bool gancanduoi(int toadox[], int toadoy[]);
+	bool checknake(int toadox[], int toadoy[]);
+	void taoqua(int &xqua, int &yqua);
 };
 void View::create_lineabove()
 {
@@ -46,9 +37,9 @@ void View::create_lineabove()
 	while (x < 75)
 	{
 		gotoXY(x, y);
-		cout << ".";
+		cout << "-";
 		x++;
-		Sleep(30);
+		Sleep(10);
 		TextColor(i);
 		i++;
 		if (i > 15)
@@ -83,9 +74,9 @@ void View::create_lineunder()
 	while (x < 75)
 	{
 		gotoXY(x, y);
-		cout << ".";
+		cout << "-";
 		x++;
-		Sleep(30);
+		Sleep(10);
 		TextColor(i);
 		i++;
 		if (i > 15)
@@ -112,7 +103,6 @@ void View::create_lineunder()
 		  }*/
 	}
 }
-
 void View::create_lineleft()
 {
 	int i = 1;
@@ -120,9 +110,9 @@ void View::create_lineleft()
 	while (y < 24)
 	{
 		gotoXY(x, y);
-		cout << ".";
+		cout << "-";
 		y++;
-		Sleep(30);
+		Sleep(10);
 		TextColor(i);
 		i++;
 		if (i > 15)
@@ -138,9 +128,9 @@ void View::create_lineright()
 	while (y < 24)
 	{
 		gotoXY(x, y);
-		cout << ".";
+		cout << "-";
 		y++;
-		Sleep(30);
+		Sleep(10);
 		TextColor(i);
 		i++;
 		if (i > 15)
@@ -149,7 +139,7 @@ void View::create_lineright()
 		}
 	}
 }
-void Snake::create_snack(int toadox[], int toadoy[])
+void View::create_snack(int toadox[], int toadoy[])
 {
 	int x = 50, y = 13;
 	for (int i = 0; i < nub; i++)
@@ -159,7 +149,7 @@ void Snake::create_snack(int toadox[], int toadoy[])
 		x--;
 	}
 }
-void Snake::draw_snack(int toadox[], int toadoy[])
+void View::draw_snack(int toadox[], int toadoy[])
 {
 	for (int i = 0; i < nub; i++)
 	{
@@ -174,7 +164,7 @@ void Snake::draw_snack(int toadox[], int toadoy[])
 		}
 	}
 }
-void Snake::Delete_snack(int toadox[], int toadoy[])
+void View::Delete_snack(int toadox[], int toadoy[])
 {
 	for (int i = 0; i < nub; i++)
 	{
@@ -182,7 +172,7 @@ void Snake::Delete_snack(int toadox[], int toadoy[])
 		cout << " ";
 	}
 }
-void Snake::process_snack(int toadox[], int toadoy[], int x, int y)
+void View::process_snack(int toadox[], int toadoy[], int x, int y)
 {
 	// b1: them toa do moi vao dau mang 
 	Add(toadox, x);
@@ -193,7 +183,7 @@ void Snake::process_snack(int toadox[], int toadoy[], int x, int y)
 	// b3: ve ran
 	draw_snack(toadox, toadoy);
 }
-void Snake::Add(int a[], int x)
+void View::Add(int a[], int x)
 {
 	for (int i = nub; i > 0; i--)
 	{
@@ -202,7 +192,7 @@ void Snake::Add(int a[], int x)
 	a[0] = x;
 	nub++;
 }
-void Snake::Delete(int a[], int vt)
+void View::Delete(int a[], int vt)
 {
 	for (int i = vt; i < nub; i++)
 	{
@@ -210,17 +200,67 @@ void Snake::Delete(int a[], int vt)
 	}
 	nub--;
 }
-
-
-void View::menu() {//tao menu game
+bool ktran(int x0, int y0)
+{
+	if (y0 == 2 && (x0 >= 10 && x0 <= 75))
+	{
+		return true; 
+	}
+	else if (y0 == 23 && (x0 >= 5 && x0 <= 75))
+	{
+		return true;
+	}
+	else if (x0 == 5 && (y0 >= 1 && x0 <= 23))
+	{
+		return true;
+	}
+	else if (x0 == 75 && (y0 >= 1 && x0 <= 23))
+	{
+		return true; 
+	}
+	return false;
+}
+bool gancanduoi(int toadox[], int toadoy[])
+{
+	for (int i = 1; i < nub; i++)
+	{
+		if ((toadox[0] == toadox[i]) && (toadoy[0] == toadoy[i]))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+bool checknake(int toadox[], int toadoy[])
+{
+	bool check_snake1 = ktran(toadox[0], toadoy[0]);
+	bool check_snake2 = gancanduoi(toadox, toadoy);
+	if (check_snake1 == true || check_snake1 == true)
+	{ 
+		gotoXY(35, 13);
+		cout << "GAME OVER";
+		return true;
+	} 
+	return false; 
+}
+void taoqua(int &xqua, int &yqua)
+{
+	xqua = rand() % (74 - 11 + 1) + 11;
+	xqua = rand() % (22 - 2 + 1) + 2;
+	gotoXY(xqua, yqua);
+	cout << "0";
+}
+void View::menu() {
 	char  enter1 = 13, esc1 = 27, as2 = 80, as3 = 72, as4 = 49, as5 = 50, as6 = 51;
-	system("cls");
+	int i = 9;
+ 	system("cls");
+	TextColor(i);
 	int chon = 1, choncu = 0;
-	gotoXY(33, 3);
+	gotoXY(33, 9);
 	cout << "Choi Luon";
-	gotoXY(30, 4);
+	gotoXY(30, 10);
 	cout << "Chon Muc Do Choi";
-	gotoXY(33, 5);
+	gotoXY(33, 11);
 	cout << "Huong Dan";
 
 	while (1)
@@ -258,41 +298,33 @@ void View::menu() {//tao menu game
 		system("cls");
 		char chondokho;
 		cout << "Bam 1 2 3 chon muc do: ( dai qua nhat lam )" << endl;
-		cout << "1.De" << endl;
-		cout << "2.Binh Thuong" << endl;
-		cout << "3.Kho";
+		cout << "De" << endl;
+		cout << "Binh Thuong" << endl;
+		cout << "Kho" << endl;
+		cout << "Thoat";
 
 		do
 		{
 			chondokho = _getch();
-			if (chondokho < as4 || chondokho > as6)
+			if (chondokho = as4 && chondokho == as5 && chondokho == as6 && chondokho == esc1)
 				cout << "Chon lai di!!!" << endl;
-		} while (chondokho < as4 || chondokho > as6);
+		} while (chondokho == as4 && chondokho == as5 && chondokho == as6 && chondokho == esc1);
 		system("cls");
 		if (chondokho == as4)
 		{
-			//doKho = 150;
-			cout << "Da chon muc do DE" << endl;
+			return;
 		}
 		if (chondokho == as5)
 		{
-			//doKho = 90;
-			cout << "Da chon muc do BINH THUONG" << endl;
+			return;
 		}
-		if (chondokho == 51)
+		if (chondokho == as6)
 		{
-			//doKho = 40;
-			cout << "Da chon muc do KHO\n" << endl;
+			return;
 		}
-		cout << "Bam enter de choi game" << endl << "Bam ESC de quay lai menu" << endl;
-		char lchon;
-		while (1)
+		if (chondokho == esc1)
 		{
-			lchon = _getch();
-			if (lchon == esc1)
-				break;
-			if (lchon == enter1)
-				return;
+			menu();
 		}
 		menu();
 	}
@@ -321,73 +353,76 @@ void View::menuchon(int chon, int choncu)
 	{
 		if (choncu == 2)
 		{
-			gotoXY(25, 4);
+			gotoXY(25, 10);
 			cout << " ";
-			gotoXY(48, 4);
+			gotoXY(50, 10);
 			cout << " ";
 		}
-		gotoXY(25, 3);
+		gotoXY(25, 9);
 		cout << mnc1;
-		gotoXY(48, 3);
+		gotoXY(50, 9);
 		cout << mnc2;
 	}
 	if (chon == 2)
 	{
 		if (choncu == 1)
 		{
-			gotoXY(25, 3);
+			gotoXY(25, 9);
 			cout << " ";
-			gotoXY(48, 3);
+			gotoXY(50, 9);
 			cout << " ";
 		}
 		if (choncu == 3)
 		{
-			gotoXY(25, 5);
+			gotoXY(25, 11);
 			cout << " ";
-			gotoXY(48, 5);
+			gotoXY(50, 11);
 			cout << " ";
 		}
-		gotoXY(25, 4);
+		gotoXY(25, 10);
 		cout << mnc1;
-		gotoXY(48, 4);
+		gotoXY(50, 10);
 		cout << mnc2;
 	}
 	if (chon == 3)
 	{
 		if (choncu == 2)
 		{
-			gotoXY(25, 4);
+			gotoXY(25, 10);
 			cout << " ";
-			gotoXY(48, 4);
+			gotoXY(50, 10);
 			cout << " ";
 		}
-		gotoXY(25, 5);
+		gotoXY(25, 11);
 		cout << mnc1;
-		gotoXY(48, 5);
+		gotoXY(50, 11);
 		cout << mnc2;
 	}
 }
 int main()    
 {
+
 	View v;
-	Snake S;
 	v.menu();
 	system("cls");
 	srand(time(NULL));
+	int xqua = 0, yqua = 0;
+	v.taoqua(xqua, yqua);
 	v.create_lineabove();
 	v.create_lineunder();
 	v.create_lineleft();
 	v.create_lineright();
+	bool gameover = false;
 	int toadox[MAX], toadoy[MAX];
 	//create();
-	S.create_snack(toadox, toadoy);
-	S.draw_snack(toadox, toadoy);
+	v.create_snack(toadox, toadoy);
+	v.draw_snack(toadox, toadoy);
 	int x = 50, y = 13;
 	int check = 2;
 	int i = 1;
-	while (true)
+	while (gameover == false)
 	{
-		S.Delete_snack(toadox, toadoy);
+		v.Delete_snack(toadox, toadoy);
 		TextColor(i);
 		i++;
 		if (i > 15)
@@ -442,7 +477,7 @@ int main()
 		{
 			x++;
 		}
-		// kiem tra bien
+		/*// kiem tra bien
 		if (y == 22)
 		{
 			check = 1;
@@ -458,10 +493,21 @@ int main()
 		else if (x == 74)
 		{
 			check = 2;
+		}  */
+		v.process_snack(toadox, toadoy, x, y);
+		//gameover = checknake(toadox, toadoy);
+		if ( gameover = ktran(toadox[0], toadoy[0]))
+		{
+			gotoXY(35, 13);
+			cout << "GAME OVER";
 		}
-		S.process_snack(toadox, toadoy, x, y);
+		/*if (gameover = gancanduoi(toadox[0], toadoy[0]))
+		{
+			gotoXY(37, 17);
+			cout << "GAME OVER";
+		}*/
 		Sleep(100);
-	}
+	} 
 	_getch();
 	return 0;
 }
